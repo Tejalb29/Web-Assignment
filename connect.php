ddfbfd49a36e247c74857399dda9282737857191
Tejalb29 <tejalbissessur@gmail.com>
2025-02-21 17:46:52 +0400

    folder

--- connect.php
+++ connect.php
@@ -1,13 +1,12 @@
 <?php
 
 $servername = "localhost";
-$username = "tejal"; 
-$password = "2299"; 
+$username = "tejal";
+$password = "2299";
 $database = "tutoring";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-
+}
